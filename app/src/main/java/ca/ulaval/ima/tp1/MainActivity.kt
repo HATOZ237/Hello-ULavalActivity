f2e44de085f370871cbdc3734892cae894dbcaab
HATOZ237 <58201604+HATOZ237@users.noreply.github.com>
2023-02-09 02:43:04 -0500

    finished WebActivity.kt

--- app/src/main/java/ca/ulaval/ima/tp1/MainActivity.kt
+++ app/src/main/java/ca/ulaval/ima/tp1/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Parcelable
+import android.webkit.WebView
 import android.widget.Button
 import java.util.Date
 import kotlinx.parcelize.Parcelize
@@ -12,17 +13,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val UrlToLoad = "https://www.ulaval.ca/"
+        val date = Date(2000, 8,13)
+        var profil = Profil("Toukam Zanjio", "Harold Wilson", date, "HATOZ" )
+
+
         val monBoutonUlaval = findViewById<Button>(R.id.ulaval)
+        val monBoutonSiteWeb = findViewById<Button>(R.id.siteWeb)
 
         monBoutonUlaval.setOnClickListener(){
             val newActivity = Intent(applicationContext, UlavalActivity::class.java)
             startActivity(newActivity)
         }
+        monBoutonSiteWeb.setOnClickListener(){
+            val newActivity = Intent(applicationContext, WebActivity::class.java)
+            newActivity.putExtra("url", UrlToLoad)
+            startActivity(newActivity)
+        }
     }
 
-    val date = Date(2000, 8,13)
-    var profil = Profil("Toukam Zanjio", "Harold Wilson", date, "HATOZ" )
-    val UrlToLoad = "www.google.com"
 
 }
 @Parcelize
--- app/src/main/java/ca/ulaval/ima/tp1/WebActivity.kt
+++ app/src/main/java/ca/ulaval/ima/tp1/WebActivity.kt
@@ -2,10 +2,23 @@ package ca.ulaval.ima.tp1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.widget.Button
 
 class WebActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web)
+
+        val Url = intent.extras?.getString("url")
+        val monBoutonFermer = findViewById<Button>(R.id.fermerWeb)
+        val myView = findViewById<WebView>(R.id.webView)
+
+        if (Url != null) {
+            myView.loadUrl(Url)
+        }
+        monBoutonFermer.setOnClickListener(){
+            finish()
+        }
     }
 }
